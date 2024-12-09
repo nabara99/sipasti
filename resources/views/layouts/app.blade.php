cc030effb1ad43faf8990f0af4b8b159076533fb
nabara99 <rtaufiq212@gmail.com>
2024-12-10 00:24:26 +0800

    dahsboard new

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,10 +31,6 @@
         gtag('config', 'UA-94034622-3');
     </script>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css"
-        integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- END GA -->
 </head>
 
 <body>
